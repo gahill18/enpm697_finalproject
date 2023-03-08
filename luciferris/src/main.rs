0ab4da51c66f2a489697c4e55f6150dc83122d86
Garrett Hill <gahill2018@protonmail.com>
2023-03-08 17:55:34 -0500

    adding config template

--- luciferris/src/main.rs
+++ luciferris/src/main.rs
@@ -1,3 +1,16 @@
+use config::{Config, ConfigError, File, FileFormat};
+
+fn read_config(path: &str) -> Result<Config, ConfigError> {
+    let mut builder = Config::builder()
+        .set_default("default", "1")?
+        .add_source(File::new(path, FileFormat::Json))
+        //  .add_async_source(...)
+        .set_override("override", "1")?;
+
+    builder.build()
+}
+
 fn main() {
-    println!("Hello, world!");
+    let conf: Config = read_config("test.json").unwrap();
+    println!("{conf:?}");
 }
