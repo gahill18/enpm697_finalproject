154f2f2255cec5d7373c4dd89e4cf6600cbebe8e
Garrett Hill <gahill2018@pm.me>
2023-03-08 21:07:41 -0500

    adding command line argument parsing and integrating with config file

--- luciferris/src/main.rs
+++ luciferris/src/main.rs
@@ -1,16 +1,59 @@
+use std::{ops::Deref, path::PathBuf};
+
+use clap::{Parser, Subcommand};
 use config::{Config, ConfigError, File, FileFormat};
 
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    /// Sets a custom config file
+    #[arg(short, long, value_name = "FILE")]
+    config: Option<String>,
+
+    /// Turn debugging information on
+    #[arg(short, long, action = clap::ArgAction::Count)]
+    debug: u8,
+
+    #[command(subcommand)]
+    mode: Option<Modes>,
+}
+
+#[derive(Subcommand, Debug)]
+enum Modes {
+    /// does testing things
+    Test {
+        /// lists test values
+        #[arg(short, long)]
+        list: bool,
+    },
+    Snoop,
+    Encrypt,
+}
+
 fn read_config(path: &str) -> Result<Config, ConfigError> {
-    let mut builder = Config::builder()
+    let builder = Config::builder()
         .set_default("default", "1")?
         .add_source(File::new(path, FileFormat::Json))
-        //  .add_async_source(...)
         .set_override("override", "1")?;
 
     builder.build()
 }
 
 fn main() {
-    let conf: Config = read_config("test.json").unwrap();
-    println!("{conf:?}");
+    let cli = Cli::parse();
+    // Check if the user provided a config path
+    if let Some(config_path) = cli.config.as_deref() {
+        println!("{config_path:?}");
+        let conf: Config = read_config(config_path).unwrap();
+        println!("{conf:?}")
+    }
+
+    // check if the user specified a mode
+    if let Some(mode) = cli.mode {
+        match mode {
+            Modes::Snoop => println!("snoop mode"),
+            Modes::Encrypt => println!("encrypt mode"),
+            _ => println!("unknown mode {mode:?}"),
+        }
+    };
 }
